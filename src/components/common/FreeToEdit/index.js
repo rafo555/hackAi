b23827abb84d856036d2f1b21dcd2664031ca3f9
melinepoghosyan <meline.poghosyan@picsart.com>
2020-02-28 19:26:42 +0400

    Created FreeToEdit page.

--- src/components/common/FreeToEdit/index.js
+++ src/components/common/FreeToEdit/index.js
@@ -1,51 +1,11 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 
+import { data } from './data';
+
 const FreeToEdit = () => {
 
     const classes = useStyles();
-    const data = [
-        {
-            id: 1,
-            type: '',
-            url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
-        },
-        {
-            id: 2,
-            type: '',
-            url: 'https://cdn130.picsart.com/59516055092957864117',
-        },
-        {
-            id: 3,
-            type: '',
-            url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
-        },
-        {
-            id: 4,
-            type: '',
-            url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
-        },
-        {
-            id: 5,
-            type: '',
-            url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
-        },
-        {
-            id: 6,
-            type: '',
-            url: 'https://cdn130.picsart.com/59516055092957864117',
-        },
-        {
-            id: 7,
-            type: '',
-            url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
-        },
-        {
-            id: 8,
-            type: '',
-            url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
-        }
-    ];
 
     const selectImage = () => {
 
---
+++ src/components/common/Upload/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const Upload = () => {
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            Upload
+        </div>
+    );
+};
+
+const useStyles = createUseStyles({
+
+});
+
+export default Upload;
