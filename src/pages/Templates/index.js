65962b52d23d832193490ca44fb68aad8f1e7803
anigulkanyan <ani.gulkanyan@picsart.com>
2020-02-28 17:51:13 +0400

    Created Templates component.Added some changes.

--- src/pages/Templates/index.js
+++ src/pages/Templates/index.js
@@ -3,8 +3,17 @@ import { createUseStyles } from 'react-jss';
 
 const data = [{
     type: '',
-    url: '',
-}]
+    url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+},
+    {
+        type: '',
+        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+    },
+    {
+        type: '',
+        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+    }
+]
 
 const Templates = () => {
 
@@ -27,12 +36,15 @@ const useStyles = createUseStyles({
         display: 'flex',
         flexWrap: 'wrap',
         marginTop: 30,
+        flexDirection: 'row',
         justifyContent: 'space-between'
     },
     templateImgClass: {
         width: 250,
         height: 250,
-        marginBottom: 10
+        marginBottom: 10,
+        display: 'flex',
+        flexDirection: 'row',
     },
     templateImg: {
         borderRadius: 7,
