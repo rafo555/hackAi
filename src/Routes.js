d9f8ca9b7a8a38aa8a9508e62bc8100ac1a3910a
lilitpotoyan <lilit.potoyan@picsart.com>
2020-02-28 22:14:42 +0400

    some changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,22 @@
 import React, { lazy, memo, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import {useSelector} from "./store/helpers";
+import isEqual from "react-fast-compare";
+import Upload from "./components/common/Upload";
+import FreeToEdit from "./components/common/FreeToEdit";
 
 const Layout = lazy(() => import('./components/sections/Layout'));
 const Templates = lazy(() => import('./pages/Templates'));
 const Images = lazy(() => import('./pages/Images'));
 
 const Routes = () => {
+    const { activeTemplatesSideBar, activeImageSidebar } = useSelector((state) => {
+        return {
+            activeTemplatesSideBar: state.general.activeTemplatesSideBar,
+            activeImageSidebar: state.general.activeImageSidebar
+        };
+    }, isEqual);
+
     return (
         <Suspense fallback={<div>Loading</div>}>
             <Switch>
@@ -13,7 +24,7 @@ const Routes = () => {
                 <Route exact path="/images" render={() => {
                     return (
                         <Layout page={'images'}>
-                            <Images/>
+                            {activeImageSidebar === 'selected' ?  <Upload/> : (activeImageSidebar === 'freeToEdit' ? <FreeToEdit /> : <Images/>)}
                         </Layout>
                     )
                 }}/>
@@ -21,7 +32,7 @@ const Routes = () => {
                 <Route exact path="/" render={() => {
                     return (
                         <Layout page={'templates'}>
-                            <Templates/>
+                            {activeTemplatesSideBar === 'effects' ? <Templates/> : <Templates/>}
                         </Layout>
                     )
                 }}/>
