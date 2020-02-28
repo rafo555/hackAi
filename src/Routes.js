d57371fabe7bb98f50fb3398c454e7add76cf4b4
lilitpotoyan <lilit.potoyan@picsart.com>
2020-02-28 23:51:20 +0400

    some changes

--- src/Routes.js
+++ src/Routes.js
@@ -8,6 +8,7 @@ import FreeToEdit from "./components/common/FreeToEdit";
 const Layout = lazy(() => import('./components/sections/Layout'));
 const Templates = lazy(() => import('./pages/Templates'));
 const Images = lazy(() => import('./pages/Images'));
+const Result = lazy(() => import('./pages/Result'))
 
 const Routes = () => {
     const { activeTemplatesSideBar, activeImageSidebar } = useSelector((state) => {
@@ -33,6 +34,14 @@ const Routes = () => {
                     return (
                         <Layout page={'templates'}>
                             {activeTemplatesSideBar === 'effects' ? <Templates/> : <Templates/>}
+                        </Layout>
+                    )
+                }}/>
+
+                <Route exact path="/result" render={() => {
+                    return (
+                        <Layout page={'result'}>
+                            {activeTemplatesSideBar === 'effects' ? <Result/> : <Result/>}
                         </Layout>
                     )
                 }}/>
---
+++ src/pages/Result/index.js
@@ -0,0 +1,24 @@
+import React, {memo} from 'react';
+import {createUseStyles} from 'react-jss';
+import {resultData} from './data';
+
+const Result = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.result}>
+            {resultData.map(el => {
+                return (
+                    <div
+                        key={`result_${el.id}`}
+                    className={}>
+                        <img src={el.url} alt=""/>
+                    </div>)
+            })}
+        </div>
+    );
+};
+
+const useStyles = createUseStyles({});
+
+export default memo(Result);
