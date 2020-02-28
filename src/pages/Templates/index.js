1d9f4fc04b5fbe5a8a4d9d5d2b4527340a2690d3
anigulkanyan <ani.gulkanyan@picsart.com>
2020-02-28 19:10:46 +0400

    Changed styles.

--- src/pages/Templates/index.js
+++ src/pages/Templates/index.js
@@ -7,40 +7,49 @@ const Templates = () => {
     const classes = useStyles();
 
     return (
-        <div className={classes.mainContainerDiv}>
-            {templates.map((el) => {
-                return (
-                    <div key={el.id} className={classes.templateImgClass}>
-                        <img alt='img' className={classes.templateImg} src={el.url}/>
-                    </div>
-                )
-            })}
+        <div className={classes.mainContainer}>
+            <p className={classes.heading}>Choose The Template</p>
+            <div className={classes.mainContainerDiv}>
+                {templates.map((el) => {
+                    return (
+                        <div key={el.id} className={classes.templateImgClass}>
+                            <img alt='img' className={classes.templateImg} src={el.url}/>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 };
 
 const useStyles = createUseStyles({
+    mainContainer: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    heading: {
+        marginLeft: 37
+    },
     mainContainerDiv: {
         display: 'flex',
         flexWrap: 'wrap',
         marginTop: 50,
         flexDirection: 'row',
-        justifyContent: 'center',
-        marginLeft: 35,
-        marginRight: 35
+        // justifyContent: 'center',
+        marginLeft: 37,
     },
     templateImgClass: {
         marginBottom: 15,
         display: 'flex',
         flexDirection: 'row',
-        marginRight: 15,
+        marginRight: 37,
         backgroundColor: 'white'
     },
     templateImg: {
-        borderRadius: 10,
+        borderRadius: 12,
         cursor: 'pointer',
-        width: 180,
-        height: 180
+        width: 340,
+        height: 353
     }
 });
 
