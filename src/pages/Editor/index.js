be2ccac27a6ec558edc3077f0146cbaa6d33016f
rafo555 <rafoh555@gmail.com>
2020-02-29 03:37:59 +0400

    editor file

--- src/pages/Editor/index.js
+++ src/pages/Editor/index.js
@@ -1,11 +1,43 @@
 import React, { memo } from 'react';
+import { createUseStyles } from "react-jss";
 
 const Editor = () => {
 
-    return (
-        <div>Editor</div>
-    )
+    const classes = useStyles();
 
+    return (
+        <div>
+            <aside className={classes.leftAside}>
+                left
+            </aside>
+
+            <aside className={classes.rightAside}>
+                right
+            </aside>
+
+            <main>
+                main
+            </main>
+        </div>
+    )
 }
+
+const useStyles = createUseStyles({
+    leftAside: {
+        float: 'left',
+        width: 300,
+        height: '100vh'
+    },
+    rightAside: {
+        float: 'right',
+        width: 50,
+        height: '100vh'
+    },
+    main: {
+        marginLeft: 300,
+        marginRight: 50
+    }
+
+});
 
 export default memo(Editor);
