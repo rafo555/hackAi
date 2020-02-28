e55bc26e431a2442206e41549612a86370a62051
rafo555 <rafoh555@gmail.com>
2020-02-28 12:45:37 +0400

    some changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                EX
-            </header>
+
+            App
+
+
         </div>
     );
 }
