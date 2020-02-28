679443b618cab83c349b2467c5b861f461da51a3
lilitpotoyan <lilit.potoyan@picsart.com>
2020-02-28 17:09:53 +0400

    added layout sidebar header components

---
+++ src/components/sections/Layout/index.js
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+
+const Layout = () => {
+
+    return (
+        <div>
+            Layout
+        </div>
+    );
+};
+
+export default memo(Layout);
---
+++ src/components/sections/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+
+const Sidebar = () => {
+
+    return (
+        <div>
+            Sidebar
+        </div>
+    );
+};
+
+export default memo(Sidebar);
