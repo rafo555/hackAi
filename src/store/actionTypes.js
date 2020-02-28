fc4d26b1e9514c5113373b04e0600e6742edfdaa
lilitpotoyan <lilit.potoyan@picsart.com>
2020-02-28 20:46:40 +0400

    active sidebar

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -7,3 +7,5 @@ export const UNDO = 'UNDO';
 export const REDO = 'REDO';
 
 export const RESET_STATE = 'RESET_STATE';
+export const CHANGE_IMAGES_SIDEBAR = 'CHANGE_IMAGES_SIDEBAR';
+export const CHANGE_TEMPLATES_SIDEBAR = 'CHANGE_TEMPLATES_SIDEBAR';
