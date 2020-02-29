96fb2e8b7b76ca13e3c46cc0c70fe717f7c173db
lilitpotoyan <lilit.potoyan@picsart.com>
2020-02-29 09:29:36 +0400

    editor

--- src/pages/Templates/data.js
+++ src/pages/Templates/data.js
@@ -1,32 +1,33 @@
+
 export const templates = [
     {
         id: 1,
         type: 'bg',
-        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+        url: 'Screen Shot 2020-02-29 at 9.17.50 AM.png',
     },
     {
         id: 2,
         type: 'lu',
-        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+        url: 'group-14-copy.jpg',
     },
     {
         id: 3,
         type: '',
-        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+        url: 'alex-perez-vlp-lo-gtr-ie-unsplash-copy-2.jpg',
     },
     {
         id: 4,
         type: '',
-        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+        url: 'alec-favale-bsuf-7-b-2-a-9-z-4-unsplash-copy-3.jpg',
     },
     {
         id: 5,
         type: '',
-        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+        url: 'alex-perez-vlp-lo-gtr-ie-unsplash-copy-3.jpg',
     },
     {
         id: 6,
         type: '',
-        url: 'https://cdn130.picsart.com/320571728314201.jpg?type=webp&to=min&r=480',
+        url: 'alex-perez-vlp-lo-gtr-ie-unsplash-copy-4.jpg',
     }
 ];
